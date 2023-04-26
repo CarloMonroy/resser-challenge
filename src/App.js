59400fefea473d030a56484c72b861ea70068cd8
CarloMonroy <carlomonroy1997@gmail.com>
2023-04-26 13:48:31 -0600

    add loading spinner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  function fetchData() {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+      setLoading(true);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Spinner animation="border" />
     </div>
   );
 }
